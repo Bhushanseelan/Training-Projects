539f1abf2ca70159fc9df738d1978c7247af5c09
BHUSHAN S S <99343148+Bhushanseelan@users.noreply.github.com>
2024-08-03 14:41:27 +0530

    Create Employee.java

---
+++ Java+JDBC Connection Programs/Employee.java
@@ -0,0 +1,52 @@
+package collectionexample;
+
+import java.util.Date;
+
+public class Employee {
+    int id;
+    String ename;
+    float sal;
+    Date doj;
+    Date dob;
+    String des;
+    String dept;
+    String address;
+    String photo;
+
+    // Getter methods
+    public int getId() {
+        return id;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public float getSal() {
+        return sal;
+    }
+
+    public Date getDoj() {
+        return doj;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+}
